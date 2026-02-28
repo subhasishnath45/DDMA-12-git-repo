8962a114192ebd94369e4676e059c9008e90eed7
subhasishnath45 <subhasishnath45@gmail.com>
2026-02-28 13:40:26 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let worldType = "modern";
+
+let txt1 = "Hello";
+let txt2 = "World";
+
+let concatOld = txt1 + " " + worldType + " " + txt2;
+console.log(concatOld); // Output: Hello World
+
+let moderntxt1 = `Hello`;
+let moderntxt2 = `World`;
+
+let concatModern = `${moderntxt1} ${worldType} ${moderntxt2} fewewufef u few fewf ${1+2}`;
+console.log(concatModern); // Output: Hello World
+
+
+
